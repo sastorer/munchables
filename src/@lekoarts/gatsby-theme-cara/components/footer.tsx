bbdab1a6ed1fc862ae122579f207b999f8a764e7
sastorer <sastorer15@gmail.com>
2022-02-26 15:39:02 -0800

    Update footer and contact info

--- src/@lekoarts/gatsby-theme-cara/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -17,24 +17,6 @@ const Footer = () => {
           a: { color: `text` },
         }}
       >
-        
-        <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2-light_w30.png" alt="LekoArts Logo" />
-        {` `}
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          sx={{ ml: 2 }}
-          href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara"
-        >
-          Theme
-        </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de?utm_source=cara&utm_medium=Theme"
-        >
-          LekoArts
-        </Link>
       </Flex>
     </Box>
   )
