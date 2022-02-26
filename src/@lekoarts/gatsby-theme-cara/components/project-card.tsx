a53b75047eb26fa4d93decab351fc34223694583
sastorer <sastorer15@gmail.com>
2022-02-26 15:43:03 -0800

    Attempting to fix widths on mobile

--- src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -10,50 +10,53 @@ type ProjectCardProps = {
 }
 const imgStyle = {
   alignSelf: `center`,
+  maxWidth: '100%',
 };
 
 const ProjectCard = ({ link, title, children, source, bg }: ProjectCardProps) => (
-  <a
-    href={link}
-    target="_blank"
-    rel="noreferrer noopener"
-    sx={{
-      width: `100%`,
-      boxShadow: `lg`,
-      position: `relative`,
-      textDecoration: `none`,
-      borderRadius: `lg`,
-      px: 4,
-      py: [4, 5],
-      color: `white`,
-      background: bg || `none`,
-      transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
-      "&:hover": {
-        color: `white !important`,
-        transform: `translateY(-5px)`,
-        boxShadow: `xl`,
-      },
-    }}
-  >
-    <img src={source} alt="Images describing how it works" width="500" style={imgStyle}/>
-    <div
+  <div sx={{maxWidth: `100%`}}>
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer noopener"
       sx={{
-        textTransform: `uppercase`,
-        letterSpacing: `wide`,
-        pt: 4,
-        fontSize: [4, 5],
-        fontWeight: `medium`,
-        lineHeight: 1,
+        width: `100%`,
+        boxShadow: `lg`,
+        position: `relative`,
+        textDecoration: `none`,
+        borderRadius: `lg`,
+        px: 4,
+        py: [4, 5],
+        color: `white`,
+        background: bg || `none`,
+        transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+        "&:hover": {
+          color: `white !important`,
+          transform: `translateY(-5px)`,
+          boxShadow: `xl`,
+        },
       }}
     >
-      {title}
-    </div>
-    <div sx={{ opacity: 0.85, textShadow: `0 2px 10px rgba(0, 0, 0, 0.3)` }}>
-      <p>
-        {children}
-      </p>
-    </div>
-  </a>
+      <img src={source} alt="Images describing how it works" width="500" style={imgStyle}/>
+      <div
+        sx={{
+          textTransform: `uppercase`,
+          letterSpacing: `wide`,
+          pt: 4,
+          fontSize: [4, 5],
+          fontWeight: `medium`,
+          lineHeight: 1,
+        }}
+      >
+        {title}
+      </div>
+      <div sx={{ opacity: 0.85, textShadow: `0 2px 10px rgba(0, 0, 0, 0.3)` }}>
+        <p>
+          {children}
+        </p>
+      </div>
+    </a>
+  </div>
 )
 
 export default ProjectCard
