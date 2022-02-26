b01276cf6fb62bc1ee47e443067f8af0406d268e
sastorer <sastorer15@gmail.com>
2022-02-26 14:59:22 -0800

    Project heading color

--- src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -15,17 +15,17 @@ const ProjectCard = ({ link, title, children, source, bg }: ProjectCardProps) =>
     target="_blank"
     rel="noreferrer noopener"
     sx={{
-      width: `90%`,
+      width: `100%`,
       boxShadow: `lg`,
       position: `relative`,
       textDecoration: `none`,
       borderRadius: `lg`,
       px: 4,
       py: [4, 5],
-      color: `white`,
       background: bg || `none`,
       transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
       "&:hover": {
+        color: `white !important`,
         transform: `translateY(-5px)`,
         boxShadow: `xl`,
       },
